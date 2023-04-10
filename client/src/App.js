492e5d11cb1b06fb4e5aa643774127a7b543e1f8
sanskriti242 <59368657+shrey2003@users.noreply.github.com>
2023-04-10 18:12:38 +0530

    Made login,register

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,68 @@
 import logo from './logo.svg';
-import './App.css';
+import './style.scss';
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { useCallback } from 'react';
-
-
 import {
   createBrowserRouter,
   RouterProvider,
-  Route
+  Route,
+  Outlet,
 } from "react-router-dom";
 import Register from "./pages/Register"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Write from "./pages/Write"
 import Single from "./pages/Single"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
 const router = createBrowserRouter([
   {
-    path: "/test",
-    element: <div>Hello world!</div>,
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post/:id",
+        element: <Single />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+    ],
   },
   {
-    path:"/register",
-    element:<Register/>,
+    path: "/register",
+    element: <Register />,
   },
   {
-    path:"/login",
-    element:<Login/>,
-  },
-  {
-    path:"/single",
-    element:<Single/>,
-  },
-  {
-    path:"/write",
-    element:<Write/>,
+    path: "/login",
+    element: <Login />,
   },
 ]);
 
 
 function App() {
   return (
-    <div>
-      <RouterProvider router={router}/>
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
 }
