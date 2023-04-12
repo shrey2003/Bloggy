798cbf2ab3539a87e303ded6ca9a5d626ae87881
sanskriti242 <59368657+shrey2003@users.noreply.github.com>
2023-04-12 15:23:09 +0530

    Updated

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -10,7 +10,7 @@ const Write = () => {
       <div className="content">
       <input type='text' placeholder='Title' />
       <div className="editorContainer">
-      <ReactQuill theme="snow" value={value} onChange={setValue} />;
+      <ReactQuill className="editor" theme="snow" value={value} onChange={setValue} />;
       </div>
       </div>
       <div className="menu">
@@ -21,7 +21,7 @@ const Write = () => {
           
           </span>
           <input style={{display:'none'}}type="file" name="" id="file" />
-          <label htmlFor='file'>Upload Image </label>
+          <label className="file" htmlFor='file'>Upload Image </label>
           <div className="buttons">
             <button>Save as draft</button>
             <button>Update</button>
@@ -29,7 +29,25 @@ const Write = () => {
         </div>
         <div className="item">
           <h1>Category</h1>
-          <input type="radio" name="cat" value="art" />
+          <div className="cat">
+          <input type="radio" name="cat" value="art" id="art"/>
+          <label htmlFor='art'>Art</label>
+          </div>
+          <div className="cat">
+          <input type="radio" name="cat" value="science" id="science"/>
+          <label htmlFor='science'>Science</label></div>
+          <div className="cat">
+          <input type="radio" name="cat" value="technology" id="technology" />
+          <label htmlFor='technology'>Technology</label></div>
+          <div className="cat">
+          <input type="radio" name="cat" value="cinema" id="cinema" />
+          <label htmlFor='cinema'>Cinema</label></div>
+          <div className="cat">
+          <input type="radio" name="cat" value="design" id="design" />
+          <label htmlFor='design'>Design</label></div>
+          <div className="cat">
+          <input type="radio" name="cat" value="food" id="food" />
+          <label htmlFor='food'>Food</label></div>
         </div>
       </div>
     </div>
