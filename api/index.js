ffe964267eb089660571c3d4072e98ae0c1dacb5
sanskriti242 <59368657+shrey2003@users.noreply.github.com>
2023-04-17 00:37:32 +0530

    Updated

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from "express";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 const app=express()
 
 app.use(express.json())
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+
+app.get("/test",(req,res)=>{
+    res.json("It works!")
+})
 
 app.listen(8800,()=>{
     console.log("connected!")
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import Logo from "../img/logo.png";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../context/authContext";
+import Logo from "../img/logo.png";
 
 const Navbar = () => {
+  const { currentUser, logout } = useContext(AuthContext);
+
   return (
     <div className="navbar">
       <div className="container">
         <div className="logo">
-        <Link to="/">
+          <Link to="/">
           <img src={Logo} alt="" />
           </Link>
         </div>
@@ -30,15 +33,23 @@ const Navbar = () => {
           <Link className="link" to="/?cat=food">
             <h6>FOOD</h6>
           </Link>
-          <span>John</span>
-          <span>Logout</span>
-          <span className='write'>
-            <Link className='link' to="/write">Write</Link>
+          <span>{currentUser?.username}</span>
+          {currentUser ? (
+            <span onClick={logout}>Logout</span>
+          ) : (
+            <Link className="link" to="/login">
+              Login
+            </Link>
+          )}
+          <span className="write">
+            <Link className="link" to="/write">
+              Write
+            </Link>
           </span>
         </div>
       </div>
-   </div>     
+    </div>
   );
 };
 
-export default Navbar
+export default Navbar;
