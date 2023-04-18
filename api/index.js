ab63937de624657dcda945d85c87060ff9d49fbb
sanskriti242 <59368657+shrey2003@users.noreply.github.com>
2023-04-18 15:12:56 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -2,17 +2,33 @@ import express from "express";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
-const app=express()
+import cookieParser from "cookie-parser";
+import multer from "multer";
 
-app.use(express.json())
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 
-app.get("/test",(req,res)=>{
-    res.json("It works!")
-})
-
-app.listen(8800,()=>{
-    console.log("connected!")
-})
+app.listen(8800, () => {
+  console.log("Connected!");
+});
